899b0743a5377298e449ecceccf60fa9d5d51e75
AlekseyPn <aleksfers@icloud.com>
2016-08-30 20:17:21 +0500

    Сверстал фокус на форму

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@ var link = document.querySelector ('.feedback-btn');
 var popup = document.querySelector('.modal-feedback');
 var overlay = document.querySelector('.modal-overlay');
 var close = popup.querySelector('.modal-feedback-close');
+var yoname = popup.querySelector('[name=your-name]');
 
 link.addEventListener('click', function(event){
   event.preventDefault();
   popup.classList.add('modal-feedback-show');
   overlay.classList.add('modal-overlay-show');
   popup.scrollIntoView(top);
+  yoname.focus();
 });
 
 close.addEventListener('click', function(event) {
